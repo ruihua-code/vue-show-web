c9b4de5c6efb8bad88a2eac964bcab490c240fe8
Logan <zrhcode@163.com>
2023-09-01 18:28:35 +0800

    feat: 更新首页内容

--- src/views/home/HomeIndex.vue
+++ src/views/home/HomeIndex.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="home">
     <ZrhHeader></ZrhHeader>
+    <div class="banner">
+      <div class="slider">
+        <img class="slider-img" :src="currentImage" alt="" srcset="" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import ZrhHeader from '@/components/header/ZrhHeader.vue'
+import slider1 from '@/assets/images/home/law-slider-1.jpg'
+import slider2 from '@/assets/images/home/law-slider-2.jpg'
+import slider3 from '@/assets/images/home/law-slider-3.jpg'
+
+const currentImage = ref(slider1)
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  width: 100%;
+  border: 1px solid #fff;
+
+  .slider-img {
+    width: 100%;
+  }
+}
+</style>
